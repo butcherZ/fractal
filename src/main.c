f0bec0263bd827643e8dc105f9b073019adb1cae
butcherZ <daydreamnation@live.com>
2018-07-18 01:47:00 +0200

    added Julia

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ void init_mandelbrot(t_mlx *map)
 	map->f.MaxRe = 1.0; // RIGHT change to zoom
 	map->f.MinIm = -1.2; // TOP
 	map->f.MaxIm = map->f.MinIm + (map->f.MaxRe - map->f.MinRe) * IMG_HEIGHT / IMG_WIDTH; // BOTTOM
-	map->f.MaxIterations = 1;
+	map->f.MaxIterations = 80;
 	map->fac.count = 1;
 }
 
@@ -43,8 +43,7 @@ void get_color(t_mlx *map)
 	map->color.g = (int)(15*(1-t)*(1-t)*t*t*255);
 	map->color.b =  (int)(8.5*(1-t)*(1-t)*(1-t)*t*255);
 }
-
-void mandelbrot(t_mlx *map)
+void mandelbrot(t_mlx *map) //this one is julia
 {
 	t_coordinate *coord;
 	int count = 0;
@@ -55,7 +54,8 @@ void mandelbrot(t_mlx *map)
 
 	map->f.Re_factor = (map->f.MaxRe - map->f.MinRe) / (IMG_WIDTH - 1);
 	map->f.Im_factor = (map->f.MaxIm - map->f.MinIm) / (IMG_HEIGHT - 1);
-	//init_mandelbrot(map);
+	map->f.c_re = 0;
+	map->f.c_im = -0.8; //chang these two values to get various julia
 	if (map->fac.count % 30 == 0)
 	{
 			map->f.MaxIterations *= 1.2;
@@ -64,19 +64,17 @@ void mandelbrot(t_mlx *map)
 	}
 	while(y < IMG_HEIGHT)
 	{
-	    map->f.c_im = map->f.MaxIm - y * map->f.Im_factor;
 			int x = 0;
 	    while(x < IMG_WIDTH)
 	    {
-	        map->f.c_re = map->f.MinRe + x * map->f.Re_factor;
-					map->f.z_re = map->f.c_re;
-					map->f.z_im = map->f.c_im;
-	       map->f.isInside = 1;
+					map->f.z_re = 1.5 * (x - IMG_WIDTH / 2) / (0.5 * IMG_WIDTH);
+					map->f.z_im = (y - IMG_HEIGHT / 2) / (0.5 * IMG_HEIGHT);
+	       	map->f.isInside = 1;
 					map->f.n = 0;
 	        while(map->f.n < map->f.MaxIterations)
 	        {
-					map->f.z_re_square = map->f.z_re * map->f.z_re;
-					map->f.z_im_square = map->f.z_im * map->f.z_im;
+						map->f.z_re_square = map->f.z_re * map->f.z_re;
+						map->f.z_im_square = map->f.z_im * map->f.z_im;
 	            if(map->f.z_re_square + map->f.z_im_square > 4)
 	            {
 	               map->f.isInside = 0;
@@ -112,6 +110,74 @@ void mandelbrot(t_mlx *map)
 	//free(coord);
 }
 
+/*void mandelbrot(t_mlx *map)
+{
+	t_coordinate *coord;
+	int count = 0;
+	if (!(coord = malloc(sizeof(t_coordinate) * IMG_WIDTH * IMG_HEIGHT)))
+		exit(1);
+	int i = 0;
+	int y = 0;
+
+	map->f.Re_factor = (map->f.MaxRe - map->f.MinRe) / (IMG_WIDTH - 1);
+	map->f.Im_factor = (map->f.MaxIm - map->f.MinIm) / (IMG_HEIGHT - 1);
+
+	if (map->fac.count % 30 == 0)
+	{
+			map->f.MaxIterations *= 1.2;
+			printf("iterations is %d\n", map->f.MaxIterations);
+			printf("count is %d\n", map->fac.count);
+	}
+	while(y < IMG_HEIGHT)
+	{
+	    map->f.c_im = map->f.MaxIm - y * map->f.Im_factor;
+			int x = 0;
+	    while(x < IMG_WIDTH)
+	    {
+	        map->f.c_re = map->f.MinRe + x * map->f.Re_factor;
+					map->f.z_re = map->f.c_re;
+					map->f.z_im = map->f.c_im;
+	       map->f.isInside = 1;
+					map->f.n = 0;
+	        while(map->f.n < map->f.MaxIterations)
+	        {
+						map->f.z_re_square = map->f.z_re * map->f.z_re;
+						map->f.z_im_square = map->f.z_im * map->f.z_im;
+	            if(map->f.z_re_square + map->f.z_im_square > 4)
+	            {
+	               map->f.isInside = 0;
+	                break;
+	            }
+	            map->f.z_im = 2 * map->f.z_re * map->f.z_im + map->f.c_im;
+	            map->f.z_re = map->f.z_re_square - map->f.z_im_square + map->f.c_re;
+							map->f.n++;
+					}
+			get_color(map);
+// //https://solarianprogrammer.com/2013/02/28/mandelbrot-set-cpp-11/
+
+		  if(map->f.isInside != 1)
+			{
+					if (i < IMG_WIDTH * IMG_HEIGHT)
+					{
+						coord[i].x = x;
+						coord[i].y = y;
+						img_put_pixel(map, coord[i].x, coord[i].y, createRGB(map->color.r, map->color.g, map->color.b));
+
+					//	printf("coord x is %d, coord y is %d\n", coord[i].x, coord[i].y);
+						//printf("i is %d\n", i);
+						i++;
+					}
+					map->coord = coord;
+					map->total = i;
+			}
+		//	printf("coord x is %d, coord y is %d\n", coord[i].x, coord[i].y);
+			x++;
+	    }
+			y++;
+	}
+	//free(coord);
+}*/
+
 void draw(t_mlx *map)
 {
 			int i = 0;
@@ -133,8 +199,7 @@ int			mlx_while(t_mlx *map)
 		if (map->f.MaxIterations <= 30)
 			map->f.MaxIterations += 1;
 		if (map->f.MaxIterations == 30)
-			map->f.MaxIterations = 1;
-
+			map->f.MaxIterations -= 1;
 		printf("iterations is %d\n", map->f.MaxIterations);
 		empty(map);
 		mandelbrot(map);
